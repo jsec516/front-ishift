059c91dfce9fffade511f1560a6884f757444882
Jahidul Islam <jsec516@gmail.com>
2016-03-24 23:25:30 +0600

    updated routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,16 +4,16 @@
 import {Component, AfterViewInit, OnInit} from 'angular2/core';
 import {RouteConfig, Router, AsyncRoute, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS} from 'angular2/common';
-// import {ResourceCmp} from './resource/core';
+import {ResourceCmp} from './resource/core';
 import {Login, AuthService} from './auth/core';
-// import {DashboardCmp} from './dashboard/core';
-// import {PatientCmp} from './patient/core';
-// import {StaffCmp} from './staff/core';
-// import {VacationCmp} from './vacation/core';
-// import {CalendarCmp} from './calendar/core';
-// import {TreatmentCmp} from './treatment/core';
-// import {EventCmp} from './event/core';
-// import {ClinicConfig} from './clinic/core';
+import {DashboardCmp} from './dashboard/core';
+import {PatientCmp} from './patient/core';
+import {StaffCmp} from './staff/core';
+import {VacationCmp} from './vacation/core';
+import {CalendarCmp} from './calendar/core';
+import {TreatmentCmp} from './treatment/core';
+import {EventCmp} from './event/core';
+import {ClinicConfig} from './clinic/core';
 import {NavTop, NavLeft, NavRight} from './navigation/core';
 
 declare var $: any;
@@ -47,189 +47,44 @@ declare var $: any;
     </div>
   `
 })
-
+// TODO: routing data can be retrieved from another function so that it doesn't look clutter
+// TODO: async routing for optimization
 @RouteConfig([
     { path: '/', name: 'Default', component: Login, useAsDefault: true },
     { path: '/login', name: 'Login', component: Login },
-    new AsyncRoute({
-        path: '/dashboard',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        // loader: () => require('./dashboard/core').then(m => m.DashboardCmp),
-        name: 'Dashboard'
-    }),
-    // { path: '/dashboard', name: 'Dashboard', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/patient/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./patient/core'], require => resolve(require('./patient/core').PatientCmp));
-        }),
-        name: 'Patient'
-    }),
-    // { path: '/patient/...', name: 'Patient', component: PatientCmp },
-    new AsyncRoute({
-        path: '/staff/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./staff/core'], require => resolve(require('./staff/core').StaffCmp));
-        }),
-        name: 'Staff'
-    }),
-    // { path: '/staff/...', name: 'Staff', component: StaffCmp },
-    new AsyncRoute({
-        path: '/appt/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./event/core'], require => resolve(require('./event/core').EventCmp));
-        }),
-        name: 'Event'
-    }),
-    // { path: '/appt/...', name: 'Event', component: EventCmp },
-    new AsyncRoute({
-        path: '/calendar',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./calendar/core'], require => resolve(require('./calendar/core').CalendarCmp));
-        }),
-        name: 'Calendar'
-    }),
-    // { path: '/calendar', name: 'Calendar', component: CalendarCmp },
-    new AsyncRoute({
-        path: '/list/upcoming',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'Upcoming'
-    }),
-    // { path: '/list/upcoming', name: 'Upcoming', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/list/completed',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'Completed'
-    }),
-    // { path: '/list/completed', name: 'Completed', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/list/cancelled',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'Cancelled'
-    }),
-    // { path: '/list/cancelled', name: 'Cancelled', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/list/wait',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'WaitList'
-    }),
-    // { path: '/list/wait', name: 'WaitList', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/service/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./treatment/core'], require => resolve(require('./treatment/core').TreatmentCmp));
-        }),
-        name: 'Service'
-    }),
-    // { path: '/service/...', name: 'Service', component: TreatmentCmp },
-    new AsyncRoute({
-        path: '/vacation/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./vacation/core'], require => resolve(require('./vacation/core').VacationCmp));
-        }),
-        name: 'Vacation'
-    }),
-    // { path: '/vacation/...', name: 'Vacation', component: VacationCmp },
-    new AsyncRoute({
-        path: '/reminder',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'Reminder'
-    }),
-    // { path: '/reminder', name: 'Reminder', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/resource/...',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./resource/core'], require => resolve(require('./resource/core').ResourceCmp));
-        }),
-        name: 'Resource'
-    }),
-    // { path: '/resource/...', name: 'Resource', component: ResourceCmp },
-    new AsyncRoute({
-        path: '/config/clinic',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./clinic/core'], require => resolve(require('./clinic/core').ClinicConfig));
-        }),
-        name: 'ClinicConfig'
-    }),
-    // { path: '/config/clinic', name: 'ClinicConfig', component: ClinicConfig },
-    new AsyncRoute({
-        path: '/config/site',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'SiteConfig'
-    }),
-    // { path: '/config/site', name: 'SiteConfig', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/et/client',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'ClientText'
-    }),
-    // { path: '/et/client', name: 'ClientText', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/et/staff',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'StaffText'
-    }),
-    // { path: '/et/staff', name: 'StaffText', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/gcal',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'GoogleCalendar'
-    }),
-    // { path: '/gcal', name: 'GoogleCalendar', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/site',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'SitePromotion'
-    }),
-    // { path: '/site', name: 'SitePromotion', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/myaccount',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'MyAccount'
-    }),
-    // { path: '/myaccount', name: 'MyAccount', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/billing',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'Billing'
-    }),
-    // { path: '/billing', name: 'Billing', component: DashboardCmp },
-    new AsyncRoute({
-        path: '/cancel',
-        loader: () => new Promise(resolve => {
-            (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
-        }),
-        name: 'CancelAccount'
-    })
-    // { path: '/cancel', name: 'CancelAccount', component: DashboardCmp }
+    { path: '/dashboard', name: 'Dashboard', component: DashboardCmp },
+    { path: '/patient/...', name: 'Patient', component: PatientCmp },
+    { path: '/staff/...', name: 'Staff', component: StaffCmp },
+    { path: '/appt/...', name: 'Event', component: EventCmp },
+    { path: '/calendar', name: 'Calendar', component: CalendarCmp },
+    { path: '/list/upcoming', name: 'Upcoming', component: DashboardCmp },
+    { path: '/list/completed', name: 'Completed', component: DashboardCmp },
+    { path: '/list/cancelled', name: 'Cancelled', component: DashboardCmp },
+    { path: '/list/wait', name: 'WaitList', component: DashboardCmp },
+    { path: '/service/...', name: 'Service', component: TreatmentCmp },
+    { path: '/vacation/...', name: 'Vacation', component: VacationCmp },
+    { path: '/reminder', name: 'Reminder', component: DashboardCmp },
+    { path: '/resource/...', name: 'Resource', component: ResourceCmp },
+    { path: '/config/clinic', name: 'ClinicConfig', component: ClinicConfig },
+    { path: '/config/site', name: 'SiteConfig', component: DashboardCmp },
+    { path: '/et/client', name: 'ClientText', component: DashboardCmp },
+    { path: '/et/staff', name: 'StaffText', component: DashboardCmp },
+    { path: '/gcal', name: 'GoogleCalendar', component: DashboardCmp },
+    { path: '/site', name: 'SitePromotion', component: DashboardCmp },
+    { path: '/myaccount', name: 'MyAccount', component: DashboardCmp },
+    { path: '/billing', name: 'Billing', component: DashboardCmp },
+    { path: '/cancel', name: 'CancelAccount', component: DashboardCmp }
 ])
 
+// TODO: for performance optimization routes should be asyncrhonous
+// new AsyncRoute({
+//         path: '/dashboard',
+//         loader: () => new Promise(resolve => {
+//             (<any>require).ensure(['./dashboard/core'], require => resolve(require('./dashboard/core').DashboardCmp));
+//         }),
+//         // loader: () => require('./dashboard/core').then(m => m.DashboardCmp),
+//         name: 'Dashboard'
+//     }),
 export class App implements AfterViewInit, OnInit {
     auth: AuthService;
     loggedIn: Boolean = true;
